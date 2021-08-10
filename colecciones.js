11ce28cf4b182dbd921a10f4e556937f0888cdeb
LPSoldier19 <rafael.bautista1@hotmail.es>
2021-08-10 11:31:27 -0600

    Creacion de los archivos base de las colecciones instanciadas hasta el momentos

---
+++ colecciones.js
@@ -0,0 +1,8 @@
+use 504Delivery;
+
+db.createCollection('pedidos');
+db.createCollection('carrito');
+db.createCollection('restaurantes');
+db.createCollection('motoristas');
+db.createCollection('clientes');
+db.createCollection('administrador');
