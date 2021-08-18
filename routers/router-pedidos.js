158c6182625cc180dd17562711ba48efb78fecd2
LPSoldier19 <rafael.bautista1@hotmail.es>
2021-08-18 16:01:19 -0600

    Mas funciones añadidas en el Router de Pedidos

--- routers/router-pedidos.js
+++ routers/router-pedidos.js
@@ -2,12 +2,15 @@ var express = require('express');
 var router = express.Router();
 var pedido = require('../models/pedido');
 var mongoose = require('mongoose');
-const motorista = require('../models/motorista');
 
 router.post('/nuevoPedido',function(req,res){
     let p = new pedido({
         cliente: req.body.cliente,
-        motorista: [],
+        motorista: {
+            nombreMotorista:'',
+            telefono:'',
+            idMotorista:''
+        },
         metodoPago: req.body.metodoPago,
         producto: req.body.producto,
         direccion: req.body.direccion,
@@ -36,5 +39,40 @@ router.get('/pedidosSinMotoristas',function(req,res){
         res.end();
     })
 });
+
+router.get('/:idCliente', function(req,res){
+    pedido.find({"cliente.idCliente": req.params.idCliente},{})
+    .then(result=>{
+        res.send(result);
+        res.end();
+    }).catch(error=>{
+        res.send(error);
+        res.end();
+    })
+    
+})
+
+router.get('/:idPedido/informacion', function(req,res){
+    pedido.find({_id:req.params.idPedido},{})
+    .then(result=>{
+        res.send(result[0]);
+        res.end();
+    })
+    .catch(error=>{
+        res.send(error);
+        res.end();
+    });
+});
+
+router.get('/', function(req,res){
+    pedido.find({},{})
+    .then(result=>{
+        res.send(result);
+        res.end();
+    }).catch(error=>{
+        res.send(error);
+        res.end();
+    })
+})
 
 module.exports = router;
