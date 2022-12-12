e4e5e738b280daf0736f0fe130bc11f0d3238223
Rafael Bautista <rafael.bautista1@hotmail.es>
2022-12-12 11:15:41 -0600

    Actualizacion del proyecto

--- modules/database.js
+++ modules/database.js
@@ -1,12 +1,15 @@
 var mongoose = require('mongoose');
 
-var servidor = 'localhost:27017';
+var servidor = '127.0.0.1:27017';
 var db = 'deliveryApp';
 
 class Database{
     constructor(){
         //Promesas
-        mongoose.connect(`mongodb://${servidor}/${db}`)
+        mongoose.connect(`mongodb://${servidor}/${db}`,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
         .then(()=>{
             console.log('Se conecto a mongo en la base de datos:',db);
         }).catch((error)=>{
